a6d578ee94d7c27f13d0d751552dcd77f237e579
Jay <ycjayhuang>
2015-03-03 21:33:27 -0500

    Initial Commit

---
+++ TaskSolution_v1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TaskSolution_v1
+//
+//  Created by Jay on 3/3/15.
+//  Copyright (c) 2015 Jay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
